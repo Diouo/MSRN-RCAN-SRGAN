c1859cbf15733fa22b16906066e9f8f87d4d06dc
Ziyan Guo <724528598@qq.com>
2023-04-22 22:02:18 +0800

    decrease the lr from 1e-5 to 1e-6

--- MyNet/Train/main.py
+++ MyNet/Train/main.py
@@ -23,7 +23,7 @@ parser.add_argument('--crop_size', type=int, default=256, help='crop size of the
 parser.add_argument('--batchSize', type=int, default=16, help='training batch size')
 parser.add_argument('--testBatchSize', type=int, default=1, help='testing batch size')
 parser.add_argument('--nEpochs', type=int, default=400, help='number of epochs to train for')
-parser.add_argument('--lr', type=float, default=1e-5, help='Learning Rate')
+parser.add_argument('--lr', type=float, default=1e-6, help='Learning Rate')
 parser.add_argument('--seed', type=int, default=42, help='random seed to use')
 
 # model configuration
