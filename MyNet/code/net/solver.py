74a7e14891f637c8b5d2567268dc444587611a40
Ziyan Guo <724528598@qq.com>
2023-04-25 18:24:25 +0800

    add resume mode; update test.ipynb a little

--- MyNet/code/net/solver.py
+++ MyNet/code/net/solver.py
@@ -1,5 +1,3 @@
-import random
-import numpy as np
 import os
 from math import log10
 from PIL import Image
@@ -8,10 +6,7 @@ from skimage.metrics import structural_similarity as ssim
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.backends.cudnn as cudnn
-from torch.autograd import Variable
 from torchvision.transforms import ToTensor
-from torch import autograd
 from torch.utils.tensorboard import SummaryWriter
 
 from net.model import Generator, Discriminator, VGG19
@@ -21,42 +16,39 @@ class MyNetTrainer(object):
     def __init__(self, config, training_loader, testing_loader, model_out_path):
         super(MyNetTrainer, self).__init__()
         self.GPU_IN_USE = torch.cuda.is_available()
+        self.upscale_factor = config.upscale_factor
+        self.nEpochs = config.nEpochs
+        self.num_residuals = 16
+
         self.netG = None
         self.netD = None
         self.G_lr = config.G_lr
         self.D_lr = config.D_lr
-        self.nEpochs = config.nEpochs
         self.criterionG = None
         self.criterionD = None
         self.criterionF= None
         self.optimizerG = None
         self.optimizerD = None
         self.feature_extractor = None
-        self.scheduler = None
-        self.seed = config.seed
-        self.upscale_factor = config.upscale_factor
-        self.num_residuals = 16
         self.training_loader = training_loader
         self.testing_loader = testing_loader
-        self.model_out_path = model_out_path
-        self.writer = SummaryWriter(model_out_path + '/tensorboard')
 
+        self.scheduler = None
+        self.model_out_path = model_out_path
+        self.checkpoint = config.checkpoint
+        self.writer = SummaryWriter(model_out_path + '/tensorboard')
+        
 
     def build_model(self):
+        print('\n===> Building the Model')
         if self.GPU_IN_USE:
-            # set the random number seed
-            np.random.seed(self.seed)
-            random.seed(self.seed)
-            torch.manual_seed(self.seed)
-            torch.cuda.manual_seed(self.seed)
-            cudnn.benchmark = True
-            
+
             # build Generator
             self.netG = Generator(n_residual_blocks=self.num_residuals, upsample_factor=self.upscale_factor, base_filter=64, num_channel=3).to('cuda:1')
             self.netG.weight_init(mean=0.0, std=0.2)
             self.criterionG = nn.MSELoss().to('cuda:1')
             self.optimizerG = optim.Adam(self.netG.parameters(), lr=self.G_lr)
-            self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizerG, milestones=[50, 100, 150, 200, 300, 350], gamma=0.5)  # lr decay
+            self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizerG, milestones=[50, 100, 150, 200, 300, 350], gamma=0.5)
             self.writer.add_graph(self.netG, input_to_model=torch.randn(16, 3, 32, 32).to('cuda:1'), verbose=False)
             
             # build Discriminator
@@ -64,14 +56,14 @@ class MyNetTrainer(object):
             self.netD.weight_init(mean=0.0, std=0.2)
             self.criterionD = nn.BCELoss().to('cuda:1')
             self.optimizerD = optim.Adam(self.netD.parameters(), lr=self.D_lr)
-            self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizerD, milestones=[50, 100, 150, 200, 300, 350], gamma=0.5)  # lr decay
+            self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizerD, milestones=[50, 100, 150, 200, 300, 350], gamma=0.5)
 
             # build feature extractor
             self.feature_extractor = VGG19().to('cuda:1')
 
 
     def pretrain(self):
-        print('\n===> Pretrain')
+        print('\n===> Pretraining')
         for _, (data, target) in enumerate(self.training_loader): # torch.Size([4, 3, 64, 64]), torch.Size([4, 3, 256, 256])
 
             self.netG.eval()
@@ -83,13 +75,13 @@ class MyNetTrainer(object):
             real_label = torch.ones(data.size(0), 1).to('cuda:1')
             fake_label = torch.zeros(data.size(0), 1).to('cuda:1')
 
-            d_real = self.netD(target) # 真实样本的判别概率
-            d_real_loss = self.criterionD(d_real, real_label) # 真实样本的损失
+            d_real = self.netD(target) # prob of real samples
+            d_real_loss = self.criterionD(d_real, real_label) # BCE loss of real samples
 
-            d_fake = self.netD(self.netG(data)) # 虚假样本的判别概率
-            d_fake_loss = self.criterionD(d_fake, fake_label) # 虚假样本的损失
+            d_fake = self.netD(self.netG(data)) # prob of fake samples
+            d_fake_loss = self.criterionD(d_fake, fake_label) # BCE loss of fake samples
 
-            d_total =  d_real_loss + d_fake_loss  # 总损失
+            d_total =  d_real_loss + d_fake_loss  # total loss of D
             d_total.backward()
             self.optimizerD.step()
 
@@ -116,15 +108,15 @@ class MyNetTrainer(object):
             self.netD.train()
             self.optimizerD.zero_grad()
 
-            d_real = self.netD(target) # 真实样本的判别概率
-            d_real_loss = self.criterionD(d_real, real_label) # 真实样本的损失
+            d_real = self.netD(target) # prob of real samples
+            d_real_loss = self.criterionD(d_real, real_label) # BCE loss of real samples
 
-            d_fake = self.netD(self.netG(data)) # 虚假样本的判别概率
-            d_fake_loss = self.criterionD(d_fake, fake_label) # 虚假样本的损失
+            d_fake = self.netD(self.netG(data)) # prob of fake samples
+            d_fake_loss = self.criterionD(d_fake, fake_label) # BCE loss of fake samples
 
-            d_total =  d_real_loss + d_fake_loss  # 总损失
+            d_total =  d_real_loss + d_fake_loss  # total loss of D
             d_total.backward()
-            d_train_loss += d_total.item() # 为了可视化批与整个数据集的变量
+            d_train_loss += d_total.item()
             self.optimizerD.step()
 
             # ===========================================================
@@ -134,14 +126,14 @@ class MyNetTrainer(object):
             self.netD.eval()
             self.optimizerG.zero_grad()
 
-            g_real = self.netG(data) # 虚假样本, torch.Size([4, 3, 1024, 1024])
-            g_fake = self.netD(g_real) # 虚假样本的判别概率
-            gan_loss = self.criterionD(g_fake, real_label) # 虚假样本的对抗损失
-            mse_loss = self.criterionG(g_real, target) # 虚假样本的距离损失
-            content_loss = self.feature_extractor.forward(g_real,target) # 虚假样本的感知损失
+            g_real = self.netG(data) # fake samples, torch.Size([4, 3, 1024, 1024])
+            g_fake = self.netD(g_real) # prob of fake samples
+            gan_loss = self.criterionD(g_fake, real_label) # Adversarial loss of fake samples
+            mse_loss = self.criterionG(g_real, target) # MSE loss of fake samples
+            content_loss = self.feature_extractor.forward(g_real,target) # VGG loss of fake samples
 
-            g_total = mse_loss + 1e-3 * gan_loss + 0.006 * content_loss# 总损失
-            g_train_loss += g_total.item() # 为了可视化批与整个数据集的变量
+            g_total = mse_loss + 1e-3 * gan_loss + 0.006 * content_loss # total loss of G
+            g_train_loss += g_total.item()
             g_total.backward()
             self.optimizerG.step()
             
@@ -177,22 +169,25 @@ class MyNetTrainer(object):
         return avg_psnr, avg_ssim
 
 
-    def save(self, epoch):
+    def save(self, best_psnr, best_ssim, best_epoch):
         print('     Saving')
         checkpoint={
-            'epoch':epoch,
+            'epoch':best_epoch,
             'D_state_dict':self.netD.state_dict(),
             'G_state_dict':self.netG.state_dict(),
             'optimize_state_dict_G':self.optimizerG.state_dict(),
             'optimize_state_dict_D':self.optimizerD.state_dict(),
+            'best_psnr':best_psnr,
+            'best_ssim':best_ssim,
                     }
         checkpoints_out_path = self.model_out_path +'/checkpoints/'
         if os.path.exists(checkpoints_out_path) == False:
             os.mkdir(checkpoints_out_path)
-        torch.save(checkpoint, checkpoints_out_path + str(epoch) + '_checkpoint.pkl')
+        torch.save(checkpoint, checkpoints_out_path + str(best_epoch) + '_checkpoint.pkl')
 
 
     def run(self):
+        print('\n===> Runing')
         best_psnr = 0
         best_ssim = 0
         best_epoch = 0
@@ -210,11 +205,41 @@ class MyNetTrainer(object):
                 best_psnr = temp_psnr
                 best_ssim = temp_ssim
                 best_epoch = epoch
-                self.save(epoch)
+                self.save(best_psnr, best_ssim, best_epoch)
         
-        self.save(self.nEpochs + 1)
+        self.save(self.nEpochs)
             
         return best_epoch
     
+
     def resume(self):
-        print('     Resuming')
+        print('\n===> Resuming')
+        best_psnr = 0
+        best_ssim = 0
+        best_epoch = 0
+
+        self.build_model()
+        checkpoint = torch.load(self.checkpoint, map_location='cuda:1')
+        self.netG.load_state_dict(checkpoint['G_state_dict']) 
+        self.netD.load_state_dict(checkpoint['D_state_dict']) 
+        self.optimizerG.load_state_dict(checkpoint['optimize_state_dict_G'])  
+        self.optimizerD.load_state_dict(checkpoint['optimize_state_dict_D'])  
+        start_epoch = checkpoint['epoch'] 
+
+        for epoch in range(start_epoch + 1, start_epoch + self.nEpochs + 1):
+            print("\n===> Epoch {} starts".format(epoch))
+
+            self.train(epoch)
+            temp_psnr, temp_ssim = self.test(epoch)
+            self.scheduler.step()
+
+            if temp_psnr >= best_psnr and temp_ssim >= best_ssim:
+                best_psnr = temp_psnr
+                best_ssim = temp_ssim
+                best_epoch = epoch
+                self.save(best_psnr, best_ssim, best_epoch)
+        
+        self.save(start_epoch+self.nEpochs)
+
+        return best_epoch
+
--- MyNet/code/train.py
+++ MyNet/code/train.py
@@ -1,8 +1,12 @@
-import torch
 import os
-import argparse
 import time
 import datetime
+import numpy as np
+import random
+import argparse
+
+import torch
+import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
 
 from net.solver import MyNetTrainer
@@ -16,7 +20,9 @@ os.environ["CUDA_VISIBLE_DEVICES"] = '0,1'
 parser = argparse.ArgumentParser(description='PyTorch Super Resolution')
 
 # Ttraining mode settings
-parser.add_argument('--mode', type=str, default='train', help='desicion of training mode, else == resume')
+parser.add_argument('--mode', type=str, default='resume', help='desicion of training mode, else == resume')
+parser.add_argument('--checkpoints_out_path', type=str, default='/home/guozy/BISHE/MyNet/result/baseline')
+parser.add_argument('--checkpoint', type=str, default='/home/guozy/BISHE/MyNet/result/baseline/checkpoints/328_checkpoint.pkl')
 
 # dataset settings
 parser.add_argument('--dataSet', type=str, default='DIV2K', help='desicion of dataset')
@@ -25,7 +31,7 @@ parser.add_argument('--crop_size', type=int, default=256, help='crop size of the
 # hyper-parameters
 parser.add_argument('--batchSize', type=int, default=16, help='training batch size')
 parser.add_argument('--testBatchSize', type=int, default=1, help='testing batch size')
-parser.add_argument('--nEpochs', type=int, default=400, help='number of epochs to train for')
+parser.add_argument('--nEpochs', type=int, default=172, help='number of epochs to train for')
 parser.add_argument('--G_lr', type=float, default=1e-5, help='Learning Rate')
 parser.add_argument('--D_lr', type=float, default=4e-6, help='Learning Rate')
 parser.add_argument('--seed', type=int, default=42, help='random seed to use')
@@ -34,6 +40,13 @@ parser.add_argument('--seed', type=int, default=42, help='random seed to use')
 parser.add_argument('--upscale_factor', '-uf',  type=int, default=4, help="only 2 * k")
 args = parser.parse_args()
 
+# set the random number seed
+np.random.seed(args.seed)
+random.seed(args.seed)
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+cudnn.benchmark = True
+            
 
 if __name__ == '__main__':
 
@@ -83,30 +96,28 @@ if __name__ == '__main__':
 
 
     elif args.mode == 'resume':
-        model_out_path =  '/home/guozy/BISHE/MyNet/result/2023-04-22_22:52:54'
-
         # ===========================================================
         # Set train dataset & test dataset
         # ===========================================================
         print('\n===> Loading datasets')
         train_set = get_training_set(args.upscale_factor,args.crop_size, args.dataSet)
         test_set = get_test_set(args.upscale_factor, args.crop_size, args.dataSet)
-        training_data_loader = DataLoader(dataset=train_set, batch_size=args.batchSize, shuffle=True, num_workers=4,)
-        testing_data_loader = DataLoader(dataset=test_set, batch_size=args.testBatchSize, shuffle=False)
+        training_data_loader = DataLoader(dataset=train_set, batch_size=args.batchSize, shuffle=True, num_workers=4, pin_memory=True)
+        testing_data_loader = DataLoader(dataset=test_set, batch_size=args.testBatchSize, shuffle=False, num_workers=4, pin_memory=True)
 
         # ===========================================================
         # train model
         # ===========================================================
         time_start = time.time()
-        model = MyNetTrainer(args, training_data_loader, testing_data_loader, model_out_path)
-        model.resume()
+        model = MyNetTrainer(args, training_data_loader, testing_data_loader, args.checkpoints_out_path)
+        best_epoch = model.resume()
 
         # ===========================================================
         # check how much time was used to train model
         # ===========================================================
         time_end = time.time()
         print('\n===> time cost of resuming:', time_end - time_start)  
-        with open(model_out_path + '/information.txt', 'a') as f:
-            f.write('\nepoch:{}, resuming time:{}s\n\n'.format(args.nEpochs, time_end - time_start))
+        with open(args.checkpoints_out_path + '/information.txt', 'a') as f:
+            f.write('\nepoch:{}, resuming time:{}s\n\n'.format(best_epoch, time_end - time_start))
             f.close()
 
